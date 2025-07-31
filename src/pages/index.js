fa5e1a8f59eaab3e8a31d231286eb24352e8e2ab
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2025-08-01 00:08:39 +0530

    donee

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>Welcome to MyApp</h1>
-      <p>This is the home page of yMy app. Responsive Next.js application.</p>
+      <p>This is the home page of My app. Responsive Next.js application.</p>
     </main>
   );
 }
